0690b78e601d66fb32b46260fa01ed6f6b6bef18
Pablo Tedesco <tedesco8@hotmail.com>
2019-09-29 01:55:08 -0300

    creando variables de entorno, configurando mongoose y conectando a la bd

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose');
+require('./config/db');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const router = require('./routes');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+require('dotenv').config({ path: 'variables.env'});
 const app = express();
 
 //habilitar handlebars como templay
@@ -15,6 +21,16 @@ app.set('view engine', 'handlebars');
 //archivos estaticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
 app.use('/', router());
 
-app.listen(5000);
+app.listen(process.env.PUERTO);
