f8ec662d71bc8ebb1b8e34ec5f0602deca133381
Pablo Tedesco <tedesco8@hotmail.com>
2019-09-28 23:19:43 -0300

    creando servidor, router y habilitando handlebars

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const router = require('./routes');
+
+const app = express();
+
+//habilitar handlebars como templay
+app.engine('handlebars',
+    exphbs({
+        defaultLayout: 'layout'
+    })
+);
+app.set('view engine', 'handlebars');
+
+app.use('/', router());
+
+app.listen(5000);
