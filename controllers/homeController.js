4199a27db729ecc3df355e01f6f4ba7f09b3167c
Pablo Tedesco <tedesco8@hotmail.com>
2019-11-06 00:47:54 -0300

    mostrando vacantes en el inicio

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,17 @@
-exports.mostrarTrabajos = (req, res) => {
+const mongoose = require('mongoose');
+const Vacante = mongoose.model('Vacante');
+
+exports.mostrarTrabajos = async (req, res, next) => {
+
+    const vacantes = await Vacante.find();
+
+    if(!vacantes) return next();
+
     res.render('home', {
         nombrePagina: 'EmpleosYa',
         tagline: 'Encuentra y publica empleos. EmpleosYa es la manera más rápida de conectar profesionales',
         barra: true,
-        boton: true
+        boton: true,
+        vacantes
     })
 } 
