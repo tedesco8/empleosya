2509ae827e3ae864c220a5e7f63a424e3cb7b1c0
Pablo Tedesco <tedesco8@hotmail.com>
2019-11-27 17:23:24 -0300

    creando formulario de editar perfil y guardando cambios

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -57,3 +57,23 @@ exports.formIniciarSesion = (req, res) => {
         nombrePagina: 'Inicia sesión en EmpleosYa'
     })
 }
+
+exports.formEditarPerfil = (req, res) => {
+    res.render('editar-perfil', {
+        nombrePagina: 'Edita tu perfil en EmpleosYa',
+        usuario: req.user
+    })
+}
+
+exports.editarPerfil = async(req, res) => {
+    const usuario = await Usuarios.findById(req.user._id);
+
+    usuario.nombre = req.body.nombre;
+    usuario.email = req.body.email;
+    if(req.body.password) {
+        usuario.password = req.body.password
+    }
+    await usuario.save();
+    req.flash('correcto', 'Cambios guardados correctamente');
+    res.redirect('/administracion');
+}
--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,14 @@ module.exports = () => {
         authController.verificarUsuario,
         authController.mostrarPanel
     );
+    router.get('/editar-perfil',
+        authController.verificarUsuario,
+        usuariosController.formEditarPerfil
+    );
+    router.post('/editar-perfil',
+        authController.verificarUsuario,
+        usuariosController.editarPerfil
+    )
 
     return router;
 }
